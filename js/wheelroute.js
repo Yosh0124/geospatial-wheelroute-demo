858eaeaec42eeacdb098b1b7ab2abc8a5323a25c
Yosh0124 <yanagi.yoshio@creatorslab.jp>
2019-02-17 12:28:34 +0900

    create base script

--- js/wheelroute.js
+++ js/wheelroute.js
@@ -1,10 +1,40 @@
-var map = L.map('map', {
-  center: [35.681,139.763],
-  zoom: 14,
-  zoomControl: true
-});
+const color_available_line = "#FF0000";
+const color_unavailable_line = "#0000FF";
 
+// Set map view
+var map = L.map('map').setView([34.6992,135.4968], 16);
 
+// Set open street map.
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-
-
+// Draw lines
+for( var i=0; i<link.features.length; i++ ) {
+  switch(link.features[i].properties.link_id) {
+    case "379bfccb-4662-48af-9714-97a4e5ad8c61":
+    case "":
+    case "":
+    case "":
+    case "":
+    case "":
+    case "":
+      var Line = L.polyline([
+        [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
+        [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
+      ],{
+        "color": color_unavailable_line,
+        "weight": 1
+      }).addTo(map);
+      break;
+    default:
+      var Line = L.polyline([
+        [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
+        [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
+      ],{
+        "color": color_available_line,
+        "weight": 1
+      }).addTo(map);
+      break;
+  }
+}
