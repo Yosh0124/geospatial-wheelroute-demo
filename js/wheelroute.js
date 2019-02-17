f7e8d513dd402e90176168c7196f45dd1d5e9d63
Yosh0124 <yanagi.yoshio@creatorslab.jp>
2019-02-17 14:44:49 +0900

    add routining

--- js/wheelroute.js
+++ js/wheelroute.js
@@ -14,51 +14,45 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 var lines = Array(i<link.features.length);
 for( var i=0; i<link.features.length; i++ ) {
 
+  var start_id = link.features[i].properties.start_id;
+  var end_id = link.features[i].properties.end_id;
+
   var is_underground = false;
-  for( var i=0; i<node.features.length; i++ ) {
-    if( node.features[i].properties.floor < 0) {
-      is_underground = true
-    }
-  }
-  if( is_underground ) {
-    if(link.features[i].properties.elevator != 1) {
-      lines[i] = L.polyline([
-        [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
-        [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
-      ],{
-        "color": color_other_line,
-        "weight": 1
-      }).addTo(map);
-      // L.marker([link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]]).addTo(map);
-      // console.log(link.features[i]);
-    } else {
-      switch(link.features[i].properties.link_id) {
-        case "379bfccb-4662-48af-9714-97a4e5ad8c61":
-          lines[i] = L.polyline([
-            [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
-            [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
-          ],{
-            "color": color_unavailable_line,
-            "weight": 1
-          }).addTo(map);
-          L.marker([link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]]).addTo(map);
-          break;
-        default:
-          lines[i] = L.polyline([
-            [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
-            [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
-          ],{
-            "color": color_available_line,
-            "weight": 1
-          }).addTo(map);
-          break;
+  for( var j=0; j<node.features.length; j++ ) {
+    if( node.features[j].properties.node_id == start_id || node.features[j].properties.node_id == end_id ){
+      if( node.features[j].properties.floor < 0 && node.features[j].properties.in_out == 3) {
+        is_underground = true
+        break
       }
     }
   }
 
-  lines[i].on('click', function(){
-    alert("link_id=" + link.features[i].properties.link_id);
-  });
+  if( is_underground ) {
+
+
+
+    // switch(link.features[i].properties.link_id) {
+      // case "379bfccb-4662-48af-9714-97a4e5ad8c61":
+      //   lines[i] = L.polyline([
+      //     [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
+      //     [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
+      //   ],{
+      //     "color": color_unavailable_line,
+      //     "weight": 1
+      //   }).addTo(map);
+      //   L.marker([link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]]).addTo(map);
+      //   break;
+      // default:
+      //   lines[i] = L.polyline([
+      //     [link.features[i].geometry.coordinates[0][1],link.features[i].geometry.coordinates[0][0]],
+      //     [link.features[i].geometry.coordinates[1][1],link.features[i].geometry.coordinates[1][0]],
+      //   ],{
+      //     "color": color_available_line,
+      //     "weight": 1
+      //   }).addTo(map);
+      //   break;
+    // }
+  }
 }
 
 // Insert markers here.
@@ -67,5 +61,13 @@ L.marker([34.700173 ,135.496104]).addTo(map);
 // Insert lines here.
 L.marker([34.704431 ,135.496943]).addTo(map);
 
-
+L.Routing.control({
+  waypoints: [
+    L.latLng(34.700173 ,135.496104),
+    L.latLng(34.704431 ,135.496943)
+  ],
+  router: new L.Routing.OSRMv1({
+    profile: "foot"
+  })
+}).addTo(map);
 
